f5646c03c68873ce2579034a1566b8722bafbe50
yrakhi093 <yrakhi093@gmail.com>
2023-10-16 16:56:32 +0530

    updated cors above to cookie parser

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cookieParser(""))
 app.use(cors());
+app.use(cookieParser(""))
 app.use(router);
 
 
