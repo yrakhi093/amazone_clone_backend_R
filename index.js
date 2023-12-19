405aef767eb83f7f1e7f9fc263817a6665dc98be
yrakhi093 <yrakhi093@gmail.com>
2023-12-19 15:54:40 +0530

    frontend deployed on render

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 // app.use(cors({credentials: true}));
-app.use(cors({credentials: true, origin: 'http://localhost:3000' || "https://657ae8281ef2c206c718553b--teal-arithmetic-393dcc.netlify.app"}));
+app.use(cors({ credentials: true, origin:"https://amazon-clone-frontend-dbgx.onrender.com"}));
 app.use(cookieParser())
 
 
